15e86d2eaaf6d1eacc413106ca75454b736cfa47
TomasMusi <tomas.musilmt@gmail.com>
2024-12-10 18:35:04 +0100

    done

---
+++ src/routes/+page.svelte
@@ -0,0 +1,49 @@
+<!-- Autor: Tomáš Musil -->
+<script lang="ts">
+</script>
+
+<title>Úvodní Stránka Pokemon Search</title>
+
+<!-- Navbar Container -->
+<nav>
+	<div class="flex h-16 w-full flex-row justify-center rounded-sm bg-blue-400">
+		<div class="flex flex-row items-center justify-center gap-8">
+			<a
+				class="rounded-md text-2xl hover:bg-yellow-300 hover:p-3 hover:transition-all hover:duration-300"
+				href="#">Domu</a
+			>
+			<a
+				class="rounded-md text-2xl hover:bg-yellow-300 hover:p-3 hover:transition-all hover:duration-300"
+				href="#">Ukazka</a
+			>
+			<button
+				class="cursor-pointer rounded-lg bg-white p-4 hover:bg-slate-200 hover:transition-all hover:duration-300"
+				><a href="#"></a>Search</button
+			>
+		</div>
+	</div>
+</nav>
+
+<!-- HeroPage -->
+
+<div class="relative flex">
+	<img class="z-30 h-auto w-full brightness-50 filter" src="/pokemon.webp" alt="HeroPage - Fotka" />
+	<div class="absolute z-50 flex h-full w-full flex-col items-center justify-center text-center">
+		<h1 class="text-5xl font-black text-white">Pokemon Search</h1>
+		<p class="mt-4 w-1/3 text-white">
+			Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus neque, quisquam ut rem,
+			minus iusto alias exercitationem sed debitis reiciendis fugit sequi, excepturi vero incidunt
+			veritatis delectus! Explicabo, numquam dolor!
+		</p>
+		<div class="mt-4 flex flex-row gap-4">
+			<button
+				class="rounded-md border border-solid border-red-400 bg-transparent p-4 font-medium text-white hover:scale-x-110 hover:border-blue-400 hover:transition-all hover:duration-300"
+				>Vice Informací</button
+			>
+			<button
+				class="rounded-md bg-red-400 p-4 pl-12 pr-12 font-medium text-white hover:scale-x-110 hover:bg-blue-400 hover:transition-all hover:duration-300"
+				><a href="/PokemonSearch">Hledat</a></button
+			>
+		</div>
+	</div>
+</div>
---
+++ src/routes/PokemonSearch/+page.svelte
@@ -0,0 +1,72 @@
+<!-- Autor: Tomáš Musil -->
+<script lang="ts">
+	import { json } from '@sveltejs/kit';
+	import Error from '../+error.svelte';
+
+	let pokemon = $state<
+		| {
+				name: string;
+				weight: number;
+				height: number;
+				sound: any;
+				image: string;
+		  }
+		| undefined
+	>(undefined);
+
+	async function SearchPokemon(URL: string): Promise<any> {
+		try {
+			const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${URL.toLowerCase()}`);
+			const data = await response.json();
+
+			if (data === undefined) {
+				console.log('Chyba');
+			} else {
+				pokemon = {
+					name: data.name,
+					weight: data.weight,
+					height: data.height / 10,
+					sound: data.cries.latest,
+					image: data.sprites.front_default
+				};
+			}
+		} catch (err) {
+			console.error(`Nastala Chyba: ${err}`);
+		}
+	}
+	let urlVal: string = $state('');
+</script>
+
+<title>Pokemon Searched: {pokemon?.name}</title>
+
+<h1 class="mb-12 mt-5 text-center text-8xl">Pokemon Search</h1>
+
+<div class="m-auto flex flex-row justify-center gap-1">
+	<input
+		bind:value={urlVal}
+		class="w-1/4 justify-center rounded-md border border-solid border-black p-3 text-center"
+		type="text"
+		placeholder="Zadej Jméno Pokemona"
+	/>
+
+	<button
+		onclick={() => {
+			SearchPokemon(urlVal);
+			console.log('Data se poslali!');
+		}}
+		class="cursor-pointer rounded bg-red-500 p-5 text-white hover:bg-red-700 hover:transition-all hover:duration-300"
+		type="submit">Potvrdit</button
+	>
+</div>
+
+<div
+	class="m-auto mt-7 flex w-1/2 flex-col justify-center rounded-md bg-green-400 pt-7 text-center"
+>
+	<h1 class="text-5xl">Data</h1>
+	<h1 class="text-2xl">Jméno {pokemon?.name}</h1>
+	<h1>Hmotnost {pokemon?.weight}</h1>
+	<h1>Velikost {pokemon?.height} M</h1>
+
+	<img class="m-auto flex w-1/3" src={pokemon?.image} alt="" />
+	<audio class="m-auto flex p-3" controls autoplay src={pokemon?.sound}></audio>
+</div>
